a15c398f027bc96543f087ac365dc72eec7ae85d
Runar Saur Modahl <runarmod@gmail.com>
2024-07-12 22:59:05 +0200

    export watchlist

--- TvTime2Letterboxd.py
+++ TvTime2Letterboxd.py
@@ -29,5 +29,20 @@ def get_watched_movies(df):
     return watched_df[["Title", "Year", "WatchedDate"]]
 
 
+def get_watchlist_movies(df, watched_df):
+    watchlist_df = df[df["type"] == "towatch"]
+
+    # Remove movies that are already watched based on Title and Year
+    watchlist_df = watchlist_df.merge(
+        watched_df, on=["Title", "Year"], how="left", indicator=True
+    )
+    watchlist_df = watchlist_df[watchlist_df["_merge"] == "left_only"]
+
+    return watchlist_df[["Title", "Year"]]
+
+
 watched_df = get_watched_movies(df)
 watched_df.to_csv("letterboxd.csv", index=False)
+
+watchlist_df = get_watchlist_movies(df, watched_df)
+watchlist_df.to_csv("letterboxd_watchlist.csv", index=False)
